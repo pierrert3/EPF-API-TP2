8596fdde00b117563351a3fd208516fd5f2b94bd
pierrert <pierre.rageot@gmail.com>
2024-01-02 14:33:28 +0100

    prediction good

--- services/epf-flower-data-science/src/api/routes/data.py
+++ services/epf-flower-data-science/src/api/routes/data.py
@@ -1,21 +1,59 @@
+from src.services.data import get_kaggle_data, load_kaggle_data_json, process_species_data, split_dataset, train_and_save_model, make_prediction
 from fastapi import APIRouter
-import requests
-import os
-from src.services.data import download_dataset
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
-data_directory = "src/data"
-file_path = os.path.join(data_directory, "iris_dataset.csv")
+@router.get("/data")
+def get_data():
+    try:
+        get_kaggle_data()
+    except:
+        return "Error: Couldn't download data."
 
-os.makedirs(data_directory, exist_ok=True)
+    return 'Ok'
 
-@router.get("/api/routes/data")
-def save_dataset():
-    url = "https://www.kaggle.com/datasets/uciml/iris"
-    response = requests.get(url)
+@router.get("/data/dowload")
+def load_data():
+    try:
+        dataset = load_kaggle_data_json()
+    except:
+        return "Error: Couldn't download data in JSON format."
+
+    return dataset
+
+@router.get("/data/process")
+def process_data():
+    try:
+        dataset = process_species_data()
+    except:
+        return "Error: Couldn't process the data."
+
+    return dataset
+
+@router.get("/data/split")
+def split_data():
+    try:
+        X_train, X_test, y_train, y_test = split_dataset()
+    except:
+        return "Error: Couldn't split the data."
+
+    return X_train, X_test, y_train, y_test
+
+@router.get("/data/train")
+def train_model():
+    try:
+        result = train_and_save_model()
+    except:
+        return "Error: Couldn't train the model."
+
+    return "Model trained and saved"
+
+@router.get("/data/prediction")
+def predict(SepalLengthCm: float, SepalWidthCm: float, PetalLengthCm: float, PetalWidthCm: float):
+    try: 
+        prediction = make_prediction(SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm)
+    except Exception as e:
+        return f"Error: {str(e)}"
     
-    with open("src/data/iris_dataset.csv", "wb") as f:
-        f.write(response.content)
-    
-    return {"message": "Dataset downloaded and saved successfully"}
+    return JSONResponse(content=prediction, status_code=200)
--- services/epf-flower-data-science/src/services/data.py
+++ services/epf-flower-data-science/src/services/data.py
@@ -1,8 +1,67 @@
-import pandas as pd
+import os
+from kaggle.api.kaggle_api_extended import KaggleApi
 import opendatasets as od
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+import joblib
+import json
+import pickle
+from sklearn.linear_model import LogisticRegression
+from flask import Flask, request, jsonify
+import logging
 
-def download_dataset():
+def get_kaggle_data():
+    api = KaggleApi()
+    api.authenticate()
+    api.dataset_download_files('uciml/iris', path='src/data/', unzip=True)
 
-    od.download("https://www.kaggle.com/datasets/uciml/iris")
-    
-    return "OK"
+    return {"status": "Dataset downloaded successfully"}
+
+def load_kaggle_data_json():
+    file_path = 'src/data/Iris.csv'
+    try:
+        df = pd.read_csv(file_path)
+        return df.to_dict(orient='records')
+    except FileNotFoundError:
+        return {"error": "Dataset file not found."}
+
+def process_species_data():
+    data = load_kaggle_data_json()
+    for record in data:
+        if 'Species' in record and record['Species'].startswith('Iris-'):
+            record['Species'] = record['Species'].replace('Iris-', '')
+    return data
+
+def split_dataset():
+    dataset = process_species_data() # (extract features and labels)
+    features = [list(record.values())[1:-1] for record in dataset] # ID field excluded
+    labels = [record['Species'] for record in dataset]
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+    return X_train, X_test, y_train, y_test 
+
+def train_and_save_model():
+
+    X_train, X_test, y_train, y_test = split_dataset()
+
+    # Load the parameter of the classifier
+    with open('src/config/model_parameters.json') as f:
+        parameters = json.load(f)
+
+    n_neighbors = int(parameters['n_neighbors']) 
+
+    # Train the KNeighbors model
+    KNN_model = KNeighborsClassifier(n_neighbors=n_neighbors)
+    KNN_model.fit(X_train, y_train)
+
+    if not os.path.exists('src/models'):
+        os.makedirs('src/models')
+
+    # pickle.dump(KNN_model, open('KNN_model.sav', 'wb'))
+    joblib.dump(KNN_model, 'src/models/KNN_model.pkl')
+
+def make_prediction(SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm):
+    model = joblib.load('src/models/KNN_model.pkl')
+    prediction = model.predict([[SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm]])
+
+    return prediction.tolist()
